ad189453e2958cc4afb341e0a081c8b3f497f152
이국진 <billlee297@gmail.com>
2025-05-10 10:48:55 +0900

    init: 프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="title">
+          <a href="/">Vision Prompt</a>
+        </div>
+      </div>
+      <div className="contents">
+        <div className='image-slider'>
+
+        </div>
+      </div>
+      <div className='menu'>
+        <div className="info">
+          <a href="/info">INFO</a>
+        </div>
+        <div className="list">
+          <a href="/list">LIST</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
